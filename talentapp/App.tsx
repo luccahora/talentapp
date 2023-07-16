0360e08de1b72357018eb8a38e265f5d111ab761
Lucca Hora <luccahora@gmail.com>
2023-07-16 06:34:24 -0300

    fonts

--- talentapp/App.tsx
+++ talentapp/App.tsx
@@ -1,11 +1,18 @@
 import { SignIn } from "@screens/SignIn";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </ThemeProvider>
   );
 }
