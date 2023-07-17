5d41a94896bb2bc049fa895977cc7ff7eace0214
Lucca Hora <luccahora@gmail.com>
2023-07-17 15:53:49 -0300

    chore: screen

---
+++ talentapp/src/screens/SignUp/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Container,
+  ContainerCreateAccount,
+  CreateAcconunt,
+  Description,
+  Title,
+} from "./styles";
+import Brand from "@components/Brand";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import Spacer from "@components/Spacer";
+
+export function SignUp() {
+  return (
+    <Container>
+      <Brand />
+      <Title>Sign Up</Title>
+      <Input label={"Name"} />
+      <Input label={"Surname"} />
+      <Input label={"Email"} />
+      <Input label={"Password"} secureTextEntry={true} />
+      <Input label={"Password"} secureTextEntry={true} />
+
+      <Button title={"Register"} />
+    </Container>
+  );
+}
